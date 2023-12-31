b391fc610cb1bcb444c446c389ef13acc9499e34
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2023-12-31 21:59:42 +0530

    file updated

--- registerform.js
+++ registerform.js
@@ -49,6 +49,7 @@ app.use(express.static(__dirname));
 
 // Set the view engine to EJS because we need to render dynamic content
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'index.html'))
